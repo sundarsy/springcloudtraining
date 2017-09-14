1b34c7806185f792479ea7fc42dfa82303b3fe5a
sundarsy <sundarsy@gmail.com>
2017-09-14 16:55:16 +0530

    Added Zuul Proxy

---
+++ springdatarestdemo/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+Œ
+\src/main/java/com/github/blog/sundarsy/springdatarestdemo/SpringdatarestdemoApplication.java,2/3/239cc132e85a209564778341d02ebec3e6b26088
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
---
+++ springdatarestdemo/src/main/java/com/github/blog/sundarsy/springdatarestdemo/repository/PersonRepository.java
@@ -0,0 +1,15 @@
+package com.github.blog.sundarsy.springdatarestdemo.repository;
+
+import com.github.blog.sundarsy.springdatarestdemo.model.Person;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by rg3 on 5/23/17.
+ */
+
+@Repository
+@RepositoryRestResource(path = "person", collectionResourceRel = "person")
+public interface PersonRepository extends CrudRepository<Person, Long> {
+}
